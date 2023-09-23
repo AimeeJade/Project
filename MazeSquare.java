93c805c99c3e0ab5a91f690b4d3a770941e2b784
rajbhandarya <126826820+rajbhandarya@users.noreply.github.com>
2023-09-22 21:56:31 -0500

    Create MazeSquare.java

---
+++ MazeSquare.java
@@ -0,0 +1,39 @@
+/**
+ * MazeSquare.java
+ * A helper class for maze solving assignment.
+ * Represents a single square within a rectangular maze.
+ *
+ * @author Aimee Yuan and Anika Rajbhandary
+ */
+public class MazeSquare {
+    // Instance variables of your choosing
+    String w;
+    String h;
+    String underscore;
+    int x; //location in the array 
+    int y;
+    // A constructor, taking whatever parameters you decide:
+    public MazeSquare(/* your parameters here */) {
+    }
+    public String wallType(){
+        String x //idk make some variable here
+
+        
+    }
+       
+
+    // Whatever methods you want, such as:
+    public boolean hasLeftWall() {
+       
+        return true;
+    }
+    public boolean hasRightWall() {
+        
+        return true;
+    }
+
+    public boolean hasBottomWall() {
+        return true;
+    }
+
+}
