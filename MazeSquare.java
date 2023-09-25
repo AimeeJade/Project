23b1112e5a7336fda2defde48b08d14402e68826
rajbhandarya <126826820+rajbhandarya@users.noreply.github.com>
2023-09-24 20:04:00 -0500

    Update MazeSquare.java

--- MazeSquare.java
+++ MazeSquare.java
@@ -10,56 +10,69 @@ import java.util.ArrayList;
 public class MazeSquare {
     // Instance variables of your choosing
     String shape;
-    ArrayList<String> mazeSquareArray = new ArrayList<>();
+
 
     // A constructor, taking whatever parameters you decide:
     public MazeSquare(String inputshape) {
         shape = inputshape;
-        
-
-        
     }
-    public String printShapeType() {
-        if (shape.equals("L")){
-            String LShape = ("|     \n|     \n|     \n+-----");
-            mazeSquareArray.add(LShape);
-            System.out.println(LShape);
-            return LShape;
-
-        }
-        if (shape.equals("_")){
-            String downShape = ("      \n      \n      \n+-----");
-            mazeSquareArray.add(downShape);
-            System.out.println(downShape);
-            return downShape;
-        }
-        if (shape.equals("-")){
-            String emptyShape =  ("      \n      \n      \n+     ");
-            mazeSquareArray.add(emptyShape);
-            System.out.println(emptyShape);
-            return emptyShape;
-        }
-        if (shape.equals("|")){
-            String leftShape = ("|     \n|     \n|     \n+     ");
-            mazeSquareArray.add(leftShape);
-            System.out.println(leftShape);
-            return leftShape;
-        }
-        return("");
+    
+    public String getSquareType(){
+        return shape;
     }
+
+    public void setSquareType(String x) {
+        if(x.length()!= 1){
+            System.out.println("Error (length)");
+            System.exit(1);
+        }
+        else{
+            shape = x;
+        }
+
+    }
+
+    // public String getShape() {
+    //     if (shape.equals("L")){
+    //         String LShape = ("|     \n|     \n|     \n+-----");
+    //         mazeSquareArray.add(LShape);
+    //         System.out.println(LShape);
+    //         return LShape;
+
+    //     }
+    //     if (shape.equals("_")){
+    //         String downShape = ("      \n      \n      \n+-----");
+    //         mazeSquareArray.add(downShape);
+    //         System.out.println(downShape);
+    //         return downShape;
+    //     }
+    //     if (shape.equals("-")){
+    //         String emptyShape =  ("      \n      \n      \n+     ");
+    //         mazeSquareArray.add(emptyShape);
+    //         System.out.println(emptyShape);
+    //         return emptyShape;
+    //     }
+    //     if (shape.equals("|")){
+    //         String leftShape = ("|     \n|     \n|     \n+     ");
+    //         mazeSquareArray.add(leftShape);
+    //         System.out.println(leftShape);
+    //         return leftShape;
+    //     }
+    //     return("");
+    // }
        
 
     // Whatever methods you want, such as:
-    public boolean hasLeftWall() {
-       
-        return true;
-    }
     public boolean hasRightWall() {
-        
+        // if (x = w and y <= h) {
+        //     Square[x][y];
+        //     return true;
+        // }
         return true;
     }
 
-    public boolean hasBottomWall() {
+
+    public boolean hasTopWall() {
         return true;
     }
 
